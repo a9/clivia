f8f7e68e3012c0bc538bcaed381b1571bde24e9f
kuqoi <a9@users.noreply.github.com>
2019-07-18 19:25:15 +0800

    add schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MissionModule } from './mission/mission.module'
+import { ScheduleModule } from '@nestcloud/schedule'
 
 @Module({
-  imports: [MissionModule],
+  imports: [MissionModule, ScheduleModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common'
+import { Interval, NestSchedule } from '@nestcloud/schedule'
 
 @Injectable()
-export class MissionService {}
+export class MissionService extends NestSchedule {
+  @Interval(1000)
+  job() {
+    console.log(123)
+  }
+}
